6a81fd5913f8182efd411b836916ad4e9620ba7e
Tejas Neema <tejas.neema@BRILLIO.COM>
2020-10-05 08:53:38 +0530

    updated menu item to navigate via router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
 import HomePage from './page/homepage/homepage.component'
 import './App.css';
+
+const HatsPage = () => {
+  return <div>
+  <h1>Hats Page</h1>
+  </div>
+}
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      {/* <HomePage/> */}
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop/hats' component={HatsPage}/>
     </div>
   );
 }
