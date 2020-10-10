4cb6108a62f4e1af5a96d7656b0fb1e0303ae7aa
Tejas Neema <tejas.neema@BRILLIO.COM>
2020-10-10 22:30:12 +0530

    navigate with router HOC

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import {Route} from 'react-router-dom'
 import HomePage from './page/homepage/homepage.component'
+import ShopPage from './page/shop/shop.component'
 import './App.css';
-
-const HatsPage = () => {
-  return <div>
-  <h1>Hats Page</h1>
-  </div>
-}
 
 function App() {
   return (
     <div>
       {/* <HomePage/> */}
       <Route exact path='/' component={HomePage}/>
-      <Route path='/shop/hats' component={HatsPage}/>
+      <Route path='/shop' component={ShopPage}/>
     </div>
   );
 }
